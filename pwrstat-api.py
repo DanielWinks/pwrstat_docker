83001c575f55a9e0da128b6656e73e8b226358f8
Daniel Winks <daniel.winks@gmail.com>
2019-08-15 14:35:58 -0400

    Updates

--- pwrstat-api.py
+++ pwrstat-api.py
@@ -18,8 +18,19 @@ yaml = YAML(typ="safe")
 
 VALID_IP_REGEX = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 
+class pwrstat_rest(Resource):
+    def get(self):
+        return jsonify(get_status())
 
-class pwrstat(Resource):
+
+class pwrstat_mqtt:
+    
+    def __init__(self, *args, **kwargs):
+      
+
+
+class pwrstat:
+  
     def __init__(self, *args, **kwargs):
         with open("pwrstat.yaml") as f:
             try:
@@ -31,53 +42,54 @@ class pwrstat(Resource):
             if "rest" in self.yamlconfig:
                 self.restconfig = self.yamlconfig.get("rest")
 
-        mqtt_schema = {
-            vol.Schema(
-                {
-                    vol.Required("topic"): str,
-                    vol.Optional("refresh", default=30): int,
-                    vol.Optional("qos", default=0): int,
-                    vol.Optional("retained", default=true): bool,
-                }
-            )
-        }
-        http_schema = {
-            vol.Schema(
-                {
-                    vol.Optional("port", default=5002): All(int, Range(min=1025, max=65535)),
-                    vol.Optional("http_bind_address", default="0.0.0.0"): All(
-                        str, Length(min=7, max=15), Match(VALID_IP_REGEX)
-                    ),
-                }
-            )
-        }
-        if self.mqttconfig is not None:
-            mqtt_schema(self.yamlconfig)
-            schedule.every(self.yamlconfig.get("refresh")).seconds.do(get_status)
-        if self.restconfig is not None:
-            http_schema(http_schema)
-            api.add_resource(pwrstat, "/pwrstat")  # return all parameters
-
-    def get_status(self):
-        status = (
-            subprocess.Popen(["pwrstat", "-status"], stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+        mqtt_schema = vol.Schema(
+            {
+                vol.Required("topic"): str,
+                vol.Required("refresh"): int,
+                vol.Required("qos"): int,
+                vol.Required("retained"): bool,
+            }
         )
-        statusArr = []
-        for line in status.splitlines():
-            line = line.lstrip()
-            line = line.replace(". ", ";")
-            line = line.replace(".", "")
-            line = line.split(";")
-            if len(line) > 1:
-                statusArr.append(line)
 
-        statusDict = {}
-        statusDict = {k[0]: k[1] for k in statusArr}
-        return statusDict
+        rest_schema = vol.Schema(
+            {
+                vol.Required("port"): vol.All(int, vol.Range(min=1025, max=65535)),
+                vol.Required("bind_address"): vol.All(
+                    str, vol.Length(min=7, max=15), vol.Match(VALID_IP_REGEX)
+                ),
+            }
+        )
 
-    def get(self):
-        return jsonify(self.get_status())
+        if self.mqttconfig is not None:
+            print(self.mqttconfig)
+            mqtt_schema(self.mqttconfig)
+            schedule.every(self.mqttconfig.get("refresh")).seconds.do(get_status)
 
+        if self.restconfig is not None:
+            rest_schema(self.restconfig)
+            api.add_resource(pwrstat_rest, "/pwrstat")  # return all parameters
+            app.run(
+                port=self.restconfig.get("port"),
+                host=self.restconfig.get("bind_address"),
+            )
+
+def get_status():
+    status = (
+        subprocess.Popen(["pwrstat", "-status"], stdout=subprocess.PIPE)
+        .communicate()[0]
+        .decode("utf-8")
+    )
+    statusArr = []
+    for line in status.splitlines():
+        line = line.lstrip()
+        line = line.replace(". ", ";")
+        line = line.replace(".", "")
+        line = line.split(";")
+        if len(line) > 1:
+            statusArr.append(line)
+
+    statusDict = {}
+    statusDict = {k[0]: k[1] for k in statusArr}
 
 if __name__ == "__main__":
-    app.run(port=5002, host="0.0.0.0")
+    pwrstat()
